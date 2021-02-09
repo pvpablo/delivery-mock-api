3650cd36fb86e638d4733dbe800dd6954fcd3df5
pvpablo <pablopv@google.com>
2021-02-08 20:55:55 -0600

    modifying base schema

--- script/schema.sql
+++ script/schema.sql
@@ -10,6 +10,9 @@ CREATE TABLE CITIZEN (
 CREATE TABLE REGISTRATION (
     registration_id SERIAL,
     government_id varchar(18) UNIQUE,
+    contact_number varchar(20) NOT NULL,
+    email TEXT,
+    residence_city VARCHAR(255) NOT NULL,
     FOREIGN KEY(government_id) REFERENCES CITIZEN(government_id),
     PRIMARY KEY(registration_id) 
 );
